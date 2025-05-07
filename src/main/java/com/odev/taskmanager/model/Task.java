f8ab78531f1593d3a4ce657133934c939cd0c094
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-05-07 21:26:30 +0300

    Create Task.java

---
+++ src/main/java/com/odev/taskmanager/model/Task.java
@@ -0,0 +1,42 @@
+package com.odev.taskmanager.model;
+
+public class Task {
+    private String title;
+    private String description;
+    private String priority;
+    private String userEmail;
+
+    // Getters - Setters
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
