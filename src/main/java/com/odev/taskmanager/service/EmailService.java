423897e0bdee43c0b1003ba882064a21f026790f
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-05-11 13:08:24 +0300

    Update EmailService.java

--- src/main/java/com/odev/taskmanager/service/EmailService.java
+++ src/main/java/com/odev/taskmanager/service/EmailService.java
@@ -1,8 +1,30 @@
-package com.odev.taskmanager.service;
+package com.odev.service;
 
-public interface EmailService {
-    void sendEmail(String to, String subject, String body);
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MotivationService motivationService;
+
+    public void sendReminderEmail(String to, String taskDetails) {
+        String quote = motivationService.getMotivationalQuote();
+        String subject = "Task Reminder";
+        String body = "Don't forget your task: " + taskDetails + "\n\n" +
+                      "💡 Motivation of the Day:\n" + quote;
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
 }
-@Autowired
-private MotivationService motivationService;
-
