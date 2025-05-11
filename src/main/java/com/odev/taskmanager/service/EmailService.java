4b1393b25308e522ed8aaddacc000a94fe494a20
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-05-11 13:05:03 +0300

    Update EmailService.java

--- src/main/java/com/odev/taskmanager/service/EmailService.java
+++ src/main/java/com/odev/taskmanager/service/EmailService.java
@@ -3,3 +3,6 @@ package com.odev.taskmanager.service;
 public interface EmailService {
     void sendEmail(String to, String subject, String body);
 }
+@Autowired
+private MotivationService motivationService;
+
