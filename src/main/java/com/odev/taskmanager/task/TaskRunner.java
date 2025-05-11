18d7c40c483be6e03c109b44c14edbc160cf47fd
Zeynep Topal <zezetopal.zeynep@gmail.com>
2025-05-11 23:42:53 +0300

    Create TaskRunner.java

---
+++ src/main/java/com/odev/taskmanager/task/TaskRunner.java
@@ -0,0 +1,23 @@
+package com.odev.task;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskRunner implements CommandLineRunner {
+
+    private final TaskInputService taskInputService;
+    private final TaskRepository taskRepository;
+
+    public TaskRunner(TaskInputService taskInputService, TaskRepository taskRepository) {
+        this.taskInputService = taskInputService;
+        this.taskRepository = taskRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Task newTask = taskInputService.promptUserForTask();
+        taskRepository.save(newTask);
+        System.out.println("Görev başarıyla kaydedildi: " + newTask.getTitle());
+    }
+}
