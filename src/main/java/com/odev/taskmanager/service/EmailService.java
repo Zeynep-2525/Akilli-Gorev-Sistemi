f7a6230fff3c551b8a6ca499418c66aaa2ef840a
Zeynep-2525 <zezetopal.zeynep@gmail.com>
2025-05-12 18:13:48 +0300

    update EmailService.java and others

--- src/main/java/com/odev/taskmanager/service/EmailService.java
+++ src/main/java/com/odev/taskmanager/service/EmailService.java
@@ -1,30 +1,5 @@
-package com.odev.service;
+package com.odev.taskmanager.service;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.stereotype.Service;
-
-@Service
-public class EmailService {
-
-    @Autowired
-    private JavaMailSender mailSender;
-
-    @Autowired
-    private MotivationService motivationService;
-
-    public void sendReminderEmail(String to, String taskDetails) {
-        String quote = motivationService.getMotivationalQuote();
-        String subject = "Task Reminder";
-        String body = "Don't forget your task: " + taskDetails + "\n\n" +
-                      "💡 Motivation of the Day:\n" + quote;
-
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject(subject);
-        message.setText(body);
-
-        mailSender.send(message);
-    }
+public interface EmailService {
+    void sendReminderEmail(String to, String taskDetails);
 }
--- src/main/java/com/odev/taskmanager/service/impl/EmailServiceImpl.java
+++ src/main/java/com/odev/taskmanager/service/impl/EmailServiceImpl.java
@@ -1,6 +1,8 @@
 package com.odev.taskmanager.service.impl;
 
 import com.odev.taskmanager.service.EmailService;
+import com.odev.taskmanager.service.MotivationService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -9,17 +11,26 @@ import org.springframework.stereotype.Service;
 public class EmailServiceImpl implements EmailService {
 
     private final JavaMailSender mailSender;
+    private final MotivationService motivationService;
 
-    public EmailServiceImpl(JavaMailSender mailSender) {
+    @Autowired
+    public EmailServiceImpl(JavaMailSender mailSender, MotivationService motivationService) {
         this.mailSender = mailSender;
+        this.motivationService = motivationService;
     }
 
     @Override
-    public void sendEmail(String to, String subject, String body) {
+    public void sendReminderEmail(String to, String taskDetails) {
+        String quote = motivationService.getMotivationalQuote();
+        String subject = "Task Reminder";
+        String body = "Don't forget your task: " + taskDetails + "\n\n" +
+                      " Motivation of the Day:\n" + quote;
+
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
         message.setSubject(subject);
         message.setText(body);
+
         mailSender.send(message);
     }
 }
